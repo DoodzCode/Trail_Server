558ab214aa236ec882f23797547462d294ce9865
Nigel Deans <ndeans@yahoo.com>
2025-02-11 19:27:22 -0800

    merging in wait_for_players( )

--- src/bin/server/main.rs
+++ src/bin/server/main.rs
@@ -1,14 +1,18 @@
 
+use std::io::{Read, Write};
 use std::collections::HashMap;
 use std::net::{SocketAddr, TcpListener, TcpStream};
-
+use std::sync::{Arc, Mutex};
 mod utils;
+use std::thread;
+
 
 use utils::{load_server_config_file, ServerConfig};
 
 fn main() {
     Server::start();
 }
+
 
 pub type PlayerCollection = HashMap<SocketAddr, TcpStream>;
 
@@ -24,7 +28,7 @@ pub enum ServerStatus {
 
 pub struct Server {
     status: ServerStatus,
-    players: PlayerCollection,
+    players: Arc<Mutex<PlayerCollection>>,
     number_of_players: u16,
     port: u16,
     listener: TcpListener,
@@ -34,51 +38,120 @@ impl Server {
     pub fn start() {
         let server_config: ServerConfig = load_server_config_file("src/config/server.json").unwrap();
 
+
         let mut game_server: Server = Server {
             status: ServerStatus::Starting,
-            players: HashMap::new(),
+            players: Arc::new(Mutex::new(HashMap::new())),
             number_of_players: server_config.number_of_players,
             port: server_config.port,
             listener: TcpListener::bind(("0.0.0.0", server_config.port))
                 .expect("Could not bind to port"),
         };
-
-        game_server.wait_for_players();
+        println!("Server is listening on port {}" , game_server.port);
+        // game_server.wait_for_players();
         game_server.run();
     }
 
     fn run(&mut self) {
-        self.status = ServerStatus::Running;
+
+        // This thread listens for the termination command...
+
+        // let running: Arc<Mutex<bool>> = Arc::new(Mutex::new(true));
+        // let running_clone = Arc::clone(&running);
+        // thread::spawn(move || {
+        //     let mut buffer = String::new();
+        //     std::io::stdin().read_line(&mut buffer).expect("Failed to read line");
+        //     let mut running = running_clone.lock().unwrap();
+        //     *running = false;
+        // });
+
+   
+        //for stream in self.listener.accept() {
+            // {
+            //     let running = running.lock().unwrap();
+            //     if !*running {
+            //         println!("Server is shutting down");
+            //         break;
+            //     }
+            // }
+    
+            //match stream {
+            match self.listener.accept() {
+                Ok((stream, addr)) => {
+                    println!("Client connected");
+
+                    let mut connections = self.players.lock().unwrap();
+                    connections.insert(addr, stream.try_clone().expect("Failed to clone stream"));
+                    let connections_clone = Arc::clone(&self.players);
+
+                    // spawn a new thread here
+                    thread::spawn(move || {
+                        Server::handle_client(addr.to_string(), stream, connections_clone);
+                    });    
+                }
+                Err(e) => {
+                    println!("Connection failed: {}", e);
+                }
+            }
+        }
+
+
+        fn handle_client(id: String, mut stream: TcpStream, connections: Arc<Mutex<PlayerCollection>>)  {
+    
+            let mut buffer = [0; 512];
+            loop {
+                match stream.read(&mut buffer) {
+                    Ok(bytes_read) => {
+                        if bytes_read == 0 {
+                            println!("Client disconnected");
+                            break;
+                        }
+                        let msg = String::from_utf8_lossy(&buffer[..bytes_read]);
+                        println!("Message received");
+    
+                        stream.write_all(b"Trail Server > ").expect("Could not write to stream");
+                    }
+                    Err(e) => {
+                        println!("Failed to read from stream: {}", e);
+                        break;
+                    }
+                }
+            }
+        }
+
+        // self.status = ServerStatus::Running;
 
 
         // Set up the GameConfig, save it to a file, then start up the engine?
     }
 
-    fn wait_for_players(&mut self) {
-        self.status = ServerStatus::WaitingForPlayers;
 
-        println!(
-            "[SERVER] Waiting for {} players to connect on port {}...",
-            self.number_of_players, self.port
-        );
 
-        while self.players.len() < self.number_of_players as usize {
-            match self.listener.accept() {
-                Ok((stream, addr)) => {
-                    println!("[PLAYER CONNECTED] {}", addr);
-                    self.players.insert(addr, stream);
-                    println!(
-                        "[PLAYERS CONNECTED] {}/{}",
-                        self.players.len(),
-                        self.number_of_players
-                    );
-                }
-                Err(e) => {
-                    println!("[ERROR] Could not accept connection: {}", e);
-                }
-            }
-        }
+    // fn wait_for_players(&mut self) {
+    //     self.status = ServerStatus::WaitingForPlayers;
 
-        println!("[SERVER] All players connected.");
-    }
-}
+    //     println!(
+    //         "[SERVER] Waiting for {} players to connect on port {}...",
+    //         self.number_of_players, self.port
+    //     );
+
+    //     while self.players.len() < self.number_of_players as usize {
+    //         match self.listener.accept() {
+    //             Ok((stream, addr)) => {
+    //                 println!("[PLAYER CONNECTED] {}", addr);
+    //                 self.players.insert(addr, stream);
+    //                 println!(
+    //                     "[PLAYERS CONNECTED] {}/{}",
+    //                     self.players.len(),
+    //                     self.number_of_players
+    //                 );
+    //             }
+    //             Err(e) => {
+    //                 println!("[ERROR] Could not accept connection: {}", e);
+    //             }
+    //         }
+    //     }
+
+    //     println!("[SERVER] All players connected.");
+    // }
+
